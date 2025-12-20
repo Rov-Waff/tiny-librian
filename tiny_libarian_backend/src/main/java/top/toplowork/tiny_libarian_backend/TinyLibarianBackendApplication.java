dfebb0543e7068bab2d12dc10914a2fafc603786
Rov-Waff <whanxiao885@gmail.com>
2025-12-20 16:15:40 +0800

    Knife4j与MP支持

--- tiny_libarian_backend/src/main/java/top/toplowork/tiny_libarian_backend/TinyLibarianBackendApplication.java
+++ tiny_libarian_backend/src/main/java/top/toplowork/tiny_libarian_backend/TinyLibarianBackendApplication.java
@@ -1,9 +1,11 @@
 package top.toplowork.tiny_libarian_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("top.toplowork.tiny_libarian_backend.mapper")
 public class TinyLibarianBackendApplication {
 
     public static void main(String[] args) {
