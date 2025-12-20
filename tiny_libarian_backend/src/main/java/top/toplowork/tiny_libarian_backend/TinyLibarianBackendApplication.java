26040e43aa4fb2d37cadeee2d2e17970d986126f
Rov-Waff <whanxiao885@gmail.com>
2025-12-20 12:14:43 +0800

    init spring boot backend

---
+++ tiny_libarian_backend/src/main/java/top/toplowork/tiny_libarian_backend/TinyLibarianBackendApplication.java
@@ -0,0 +1,13 @@
+package top.toplowork.tiny_libarian_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyLibarianBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyLibarianBackendApplication.class, args);
+    }
+
+}
---
+++ tiny_libarian_backend/src/test/java/top/toplowork/tiny_libarian_backend/TinyLibarianBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.toplowork.tiny_libarian_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyLibarianBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
